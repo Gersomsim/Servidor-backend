f9baec7eca3c06032c319e67b90c407889012640
Gersomsim <gersomhernandez@me.com>
2022-06-14 01:34:24 -0500

    Leyendo variables de entorno .env

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const {dbConnection} = require('./database/config');
 
 const app = express();
+
 //Base de datos
 dbConnection();
 
-//Dracka911- gersomsim
+
 // RUTAS
 app.get( '/', (req, res) => {
   res.json( {
     ok: true,
     msg: 'Hola mundo'
   } )
-});
+}); 
 
-app.listen( 3000, ()=>{
-  console.log( 'servidor corriendo en el puerto ', + 3000);
+app.listen( process.env.PORT, ()=>{
+  console.log( 'servidor corriendo en el puerto ', + process.env.PORT);
 } )
